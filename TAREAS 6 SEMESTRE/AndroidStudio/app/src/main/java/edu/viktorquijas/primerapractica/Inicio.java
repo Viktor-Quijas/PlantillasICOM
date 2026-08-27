3a9d5f91ed0a8c0bdb22e1b914012c236caabfff
Viktor-Quijas <victor.quijas2977@alumnos.udg.mx>
2026-08-26 18:21:01 -0600

    AndroidProyect agregado a 6 Semestre

---
+++ TAREAS 6 SEMESTRE/AndroidStudio/app/src/main/java/edu/viktorquijas/primerapractica/Inicio.java
@@ -0,0 +1,36 @@
+package edu.viktorquijas.primerapractica;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inicio extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_segunda_vista);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Inicio.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+
+    }
+
+
+
+}
